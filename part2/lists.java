e3919262d44d036b3ef7100a9ba338fff59b19a1
John Ramberger <jramberger3@gatech.edu>
2023-12-15 00:24:47 -0500

    Create lists.java

---
+++ part2/lists.java
@@ -0,0 +1,22 @@
+package part2;
+
+public class lists {
+  public static void main(String[] args) {
+    // type name = value
+    int[] oddnumbers = {1, 3, 5, 7, 9};
+    //[] makes our primitive thing into a list
+    String[] names = {"Alex", "Bob", "Charlie", "Diana", "Ethan"};
+    boolean[] bools = {true, false, true, false, true};
+// to access the value from a list you do another [] and you give an index such as 1, this means that we want the first index value fo the list INDEXES START AT 0
+    System.out.println(oddnumbers[0]);
+  }
+}
+
+// High Level - Low Level
+// High level = easy to read, easy to write, easy to understand: java
+// Low level = hard to read, hard to write, hard to understand: 
+
+
+
+
+
