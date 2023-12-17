270940a0659cbd82c8640023969c5ea354c9762c
John Ramberger <jramberger3@gatech.edu>
2023-12-16 23:03:07 -0500

    Update quiz1.java

--- fundamentals/quiz1.java
+++ fundamentals/quiz1.java
@@ -29,46 +29,60 @@ public class quiz1 {
     return 0;
   }
 
-  static boolean problem5_opposite(boolean a) {
+  static boolean problem5_even(int a) {
+    // TODO: Given an integer, return true if the integer is even, false otherwise.
+    // HINT: Use the % operator, ex 5 % 2 -> 1
+
+    return false;
+  }
+
+  static boolean problem6_odd(int a) {
+    // TODO: Given an integer, return true if the integer is odd, false otherwise.
+    // HINT: Use the % operator, ex 5 % 2 -> 1
+
+    return false;
+  }
+
+  static boolean problem7_opposite(boolean a) {
     // TODO: Given a boolean, return the opposite boolean.
 
     return false;
   }
 
-  static boolean problem6_atleast2(boolean a, boolean b, boolean c) {
+  static boolean problem8_atleast2(boolean a, boolean b, boolean c) {
     // TODO: Given three booleans, return true if at least two of the booleans are true.
 
     return false;
   }
 
-  static int problem7_floor(double a) {
+  static int problem9_floor(double a) {
     // TODO: Given a double, return the floor of the double. HINT: Cast the double to an int.
 
     return 0;
   }
 
-  static int problem8_round(double a) {
+  static int problem10_round(double a) {
     // TODO: Given a double, return the rounded double. 
     // You can use Math.floor() and/or Math.ceil(), but not Math.round().
 
     return 0;
   }
 
-  static String xXproblem9Xx(String a) {
+  static String xXproblem11Xx(String a) {
     // TODO: Given a string, return the string with xX before and Xx after the string.
     // HINT: no loop is needed
 
     return "";
   }
 
-  static String problem10_concatenate(String a, String b) {
+  static String problem12_concatenate(String a, String b) {
     // TODO: Given two strings, return the two strings concatenated together.
     // HINT: no loop is needed
 
     return "";
   }
 
-  static int problem11_length(String a) {
+  static int problem13_length(String a) {
     // TODO: Given a string, return the length of the string.
     // HINT: use String.split() to split the string into a list of characters.
     // ex: "hello".split("") -> ["h", "e", "l", "l", "o"]
@@ -76,60 +90,148 @@ public class quiz1 {
     return 0;
   }
 
-  static String problem12_longer(String a, String b) {
+  static String problem14_longer(String a, String b) {
     // TODO: Given two strings, return the longer string.
 
     return "";
   }
 
-  static String problem13_firstremoved(String a) {
+  static String problem15_firstremoved(String a) {
     // TODO: Given a string, return the string with the first character removed.
 
     return "";
   }
 
-  static String problem14_lastremoved(String a) {
+  static String problem16_lastremoved(String a) {
     // TODO: Given a string, return the string with the last character removed.
 
     return "";
   }
 
-  static String problem15_firstlastremoved(String a) {
+  static String problem17_firstlastremoved(String a) {
     // TODO: Given a string, return the string with the first and last characters removed.
 
     return "";
   }
 
-  static String problem16_swapped(String a) {
+  static String problem18_swapped(String a) {
     // TODO: Given a string, return the string with the first and last characters swapped.
 
     return "";
   }
 
-  static String problem17_capitalize(String a) {
+  static String problem19_capitalize(String a) {
     // TODO: Given a string, return the string with the first letter capitalized.
     // HINT: Use String.toUpperCase(), ex "hello".toUpperCase() -> "HELLO"
 
     return "";
   }
 
-  static String problem18_capitalize2(String a) {
+  static String problem20_capitalize2(String a) {
     // TODO: Given a string, return the string with the first letter capitalized and the rest lowercase.
     // HINT: Use String.toLowerCase(), ex "HELLO".toLowerCase() -> "hello"
     
     return "";
   }
 
-  static String problem19_joinspace(String a, String b) {
+  static String problem21_joinspace(String a, String b) {
     // TODO: Given two strings, return the two strings concatenated together with a space in between.
     
     return "";
   }
 
-  static String problem20_alternating(String a, String b) {
+  static String problem22_alternating(String a, String b) {
     // TODO: Given two strings, return a string with the letters alternating between the two strings.
     // ex: "abc", "xyz" -> "axbycz"
     
     return "";
+  }
+
+  static int problem23_longestlength(String a, String b, String c) {
+    // TODO: Given three strings, return the number of characters in the longest string.
+    
+    return 0;
+  }
+
+  static int[] problem24_double(int[] list) {
+    // TODO: Given an array of integers, return the array with each element multiplied by 2.
+    
+    return new int[0];
+  }
+
+  static int problem25_sum(int[] list) {
+    // TODO: Given an array of integers, return the sum of the integers.
+    
+    return 0;
+  }
+
+  static boolean[] problem26_positive(int[] list) {
+    // TODO: Given an array of integers, return an array of booleans where 
+    // each element is true if the corresponding element in the original array is positive.
+    // ex: [1, -2, 3, -4, 0] -> [true, false, true, false, true]
+    
+    return new boolean[0];
+  }
+
+  static boolean[] problem27_even(int[] list) {
+    // TODO: Given an array of integers, return an array of booleans where
+    // each element is true if the corresponding element in the original array is even.
+    // ex: [0, 1, 2, 3, 4] -> [true, false, true, false, true]
+    // HINT: Use the % operator, ex 5 % 2 -> 1
+    
+    return new boolean[0];
+  }
+
+  static int[] problem28_floor(double[] list) {
+    // TODO: Given an array of doubles, return an array of integers where
+    // each element is the floor of the corresponding element in the original array.
+    // ex: [1.1, 2.2, 3.3] -> [1, 2, 3]
+    // HINT: Cast each element to an int.
+    
+    return new int[0];
+  }
+
+  static int problem29_counttrue(boolean[] list) {
+    // TODO: Given an array of booleans, return the count of elements that are true.
+    
+    return 0;
+  }
+
+  static String problem29_concatenate(String[] list) {
+    // TODO: Given an array of strings, return a string with the strings concatenated together.
+    
+    return "";
+  }
+
+  static String[] problem31_xXlistXx(String[] list) {
+    // TODO: Given an array of strings, return an array of strings where
+    // each element is the string with xX before and Xx after the string.
+    // ex: ["hello", "world"] -> ["xXhelloXx", "xXworldXx"]
+    
+    return new String[0];
+  }
+
+  static String[] problem32_reverse(String[] list) {
+    // TODO: Given an array of strings, return an array of strings in reverse order.
+    // ex: ["hello", "world"] -> ["world", "hello"]
+    
+    return new String[0];
+  }
+
+  static String[] problem33_filter(String[] lista, boolean[] listb) {
+    // TODO: Given two arrays of strings and booleans, return an array of strings where
+    // each element is the string from lista if the corresponding element in listb is true.
+    // ex: ["hello", "world"], [true, false] -> ["hello"]
+    
+    return new String[0];
+  }
+
+  static double[] problem34_math(int[] lista, double[] listb, boolean[] listc) {
+    // TODO: Given three arrays of integers, doubles, and booleans, return an array of strings where
+    // each element is the sum of the integers from lista and the doubles from listb if the corresponding element in listc is true.
+    // otherwise, each element is the difference of the integers from lista and the doubles from listb.
+    // ex: [1, 2, 3], [0.1, 0.2, 0.3], [true, false, true] -> [1.1, 1.8, 3.3]
+    
+    return new double[0];
   }
 }
