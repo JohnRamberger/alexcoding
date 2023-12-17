83e00566d0342d2ce6e470ab4b34a03460fd7076
John Ramberger <jramberger3@gatech.edu>
2023-12-16 22:32:37 -0500

    Create quiz1.java

---
+++ fundamentals/quiz1.java
@@ -0,0 +1,135 @@
+package fundamentals;
+
+public class quiz1 {
+  public static void main(String[] args) {
+    
+  }
+
+  static int problem1_sum(int a, int b) {
+    // TODO: Given two integers, return the sum of the two integers.
+
+    return 0;
+  }
+
+  static int problem2_square(int a) {
+    // TODO: Given an integer, return the square of the integer.
+
+    return 0;
+  }
+
+  static double problem3_division(int numerator, int denominator) {
+    // TODO: Given two integers, return the quotient of the two integers.
+
+    return 0.0;
+  }
+
+  static int problem4_largest(int a, int b, int c) {
+    // TODO: Given three integers, return the integer which is larger.
+
+    return 0;
+  }
+
+  static boolean problem5_opposite(boolean a) {
+    // TODO: Given a boolean, return the opposite boolean.
+
+    return false;
+  }
+
+  static boolean problem6_atleast2(boolean a, boolean b, boolean c) {
+    // TODO: Given three booleans, return true if at least two of the booleans are true.
+
+    return false;
+  }
+
+  static int problem7_floor(double a) {
+    // TODO: Given a double, return the floor of the double. HINT: Cast the double to an int.
+
+    return 0;
+  }
+
+  static int problem8_round(double a) {
+    // TODO: Given a double, return the rounded double. 
+    // You can use Math.floor() and/or Math.ceil(), but not Math.round().
+
+    return 0;
+  }
+
+  static String xXproblem9Xx(String a) {
+    // TODO: Given a string, return the string with xX before and Xx after the string.
+    // HINT: no loop is needed
+
+    return "";
+  }
+
+  static String problem10_concatenate(String a, String b) {
+    // TODO: Given two strings, return the two strings concatenated together.
+    // HINT: no loop is needed
+
+    return "";
+  }
+
+  static int problem11_length(String a) {
+    // TODO: Given a string, return the length of the string.
+    // HINT: use String.split() to split the string into a list of characters.
+    // ex: "hello".split("") -> ["h", "e", "l", "l", "o"]
+
+    return 0;
+  }
+
+  static String problem12_longer(String a, String b) {
+    // TODO: Given two strings, return the longer string.
+
+    return "";
+  }
+
+  static String problem13_firstremoved(String a) {
+    // TODO: Given a string, return the string with the first character removed.
+
+    return "";
+  }
+
+  static String problem14_lastremoved(String a) {
+    // TODO: Given a string, return the string with the last character removed.
+
+    return "";
+  }
+
+  static String problem15_firstlastremoved(String a) {
+    // TODO: Given a string, return the string with the first and last characters removed.
+
+    return "";
+  }
+
+  static String problem16_swapped(String a) {
+    // TODO: Given a string, return the string with the first and last characters swapped.
+
+    return "";
+  }
+
+  static String problem17_capitalize(String a) {
+    // TODO: Given a string, return the string with the first letter capitalized.
+    // HINT: Use String.toUpperCase(), ex "hello".toUpperCase() -> "HELLO"
+
+    return "";
+  }
+
+  static String problem18_capitalize2(String a) {
+    // TODO: Given a string, return the string with the first letter capitalized and the rest lowercase.
+    // HINT: Use String.toLowerCase(), ex "HELLO".toLowerCase() -> "hello"
+    
+    return "";
+  }
+
+  static String problem19_joinspace(String a, String b) {
+    // TODO: Given two strings, return the two strings concatenated together with a space in between.
+    
+    return "";
+  }
+
+  static String problem20_alternating(String a, String b) {
+    // TODO: Given two strings, return a string with the letters alternating between the two strings.
+    // ex: "abc", "xyz" -> "axbycz"
+    
+    return "";
+  }
+}
