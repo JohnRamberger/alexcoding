79766844ec22336acf795edec28750e5425fb2d3
John Ramberger <jramberger3@gatech.edu>
2023-12-26 23:16:06 -0500

    Create a.java

---
+++ chapter2/part2/a.java
@@ -0,0 +1,23 @@
+package chapter2.part2;
+
+public class a {
+  public static void main(String[] args) {
+    System.out.println(US.a);
+    
+    US.a = 20;
+    System.out.println(country.a);
+    
+
+  }
+}
+
+class country {
+  static int a = 10;
+  protected int hello = 5;
+}
+
+class US extends country {
+  void abc () {
+    hello = 5;
+  }
+}
