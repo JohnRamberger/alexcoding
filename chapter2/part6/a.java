5c9a3b14cd6b6050e08261998ee490e715ad1275
John Ramberger <jramberger3@gatech.edu>
2023-12-28 20:49:21 -0500

    Update a.java

--- chapter2/part6/a.java
+++ chapter2/part6/a.java
@@ -1,7 +1,41 @@
 package chapter2.part6;
 
+import java.util.ArrayList;
+
 // GENERICS and enums
 
+enum boolean1 {
+  tru,
+  fals,
+  maybe
+}
+
+enum Color {
+  Red,
+  Green,
+  Blue,
+  Yellow
+}
+
 public class a {
-  
+  public static void main(String[] args) {
+    AL<Integer> list = new AL<>();
+
+    list.add(90);
+    System.out.println(list.get(0));
+  }
+}
+// defined type with specific options you can choose from, drop down menu when selecting state
+// cannot use true and false bc those are reserved variables
+
+class AL<T> {
+  ArrayList<T> arr = new ArrayList<>();
+
+  T get(int index) {
+    return arr.get(index);
+  }
+
+  void add(T value) {
+    arr.add(value);
+  }
 }
