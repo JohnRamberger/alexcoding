59d487980dce4ab857b982db03bf536954ca152d
John Ramberger <jramberger3@gatech.edu>
2024-01-08 04:30:34 -0500

    Create a.java

---
+++ chapter3/part1/a.java
@@ -0,0 +1,39 @@
+package chapter3.part1;
+import java.util.Scanner;
+
+public class a {
+  public static void main(String[] args) {
+    
+    // int x = -1;
+    // while (x < 0) {
+    //   try {
+    //     Scanner scan = new Scanner(System.in);
+    //     System.out.println("Enter your age: ");
+    //     x = scan.nextInt();
+        
+    //   } catch (IllegalStateException e) {
+    //     break;
+    //   }
+    //   catch (Exception e) {
+    //     System.out.println("You must enter a number.");
+    //   }
+    // }
+    // System.out.println("You typed in: " + (x + 1));
+
+    // switch(args[0]) {
+    //   case "add":
+    //     System.out.println("hello0");
+    //     break;
+    //   case "subtract": 
+    //     System.out.println("hello1");
+    //     break;
+    //   case "multiply": 
+    //     System.out.println("hello2");
+    //     break;
+    //   default:
+    //     System.out.println("you did not enter a valid operation");
+    //     break;
+    // }
+    
+  }
+}
